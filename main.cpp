5ef4a8b553cdca901716a95a1dda315bba12874a
Roy Bridgewater <roy.bridgewater417@gmail.com>
2022-04-29 12:39:07 -0400

    Collision testing

--- main.cpp
+++ main.cpp
@@ -17,42 +17,16 @@ int main(){
 
 	clear_space();
 
-	//block(20,10);
-	//beacon(5,5);
-	//blinker(10,5);
-	//gosper_glider_gun(1,5);
-	/*gosper_glider_gun(40,5);
-	gosper_glider_gun(79,5);
-	gosper_glider_gun(118,5);
-	gosper_glider_gun(157,5);*/
+	gosper_glider_gun(1,5);
 
-	//gosper_glider_gun_rotate90_Up(50,50);
+	gosper_glider_gun_rotate90_Up(19,80);
 	//gosper_glider_gun_rotate90(100,50);
-
-	eater(50,50);
-
-	//gosper_glider_gun_flipY(1,50);
-	/*gosper_glider_gun_flipY(40,100);
-	gosper_glider_gun_flipY(79,100);
-	gosper_glider_gun_flipY(118,100);
-	gosper_glider_gun_flipY(157,100);*/
-	//infinite(50,50);
-	/*LWSS(50,50);
-	LWSS(50,60);
-	LWSS(50,70);*/ 
-	//glider(21,5);
-	//glider(30,5);
-	//glider(40,5);
-	//glider_flip(18,15);
-	//glider_flip(30,15);
-	//glider_flip(40,10);
-	//block(26,9);
 
 	print_space();
 	refresh();
 	getch();
 
-	run_life(200);
+	run_life(1000);
 	mvaddstr(0,0,"Done");
 
 	getch();
@@ -60,4 +34,5 @@ int main(){
 
 	//debug();
 	//another git test
+	//test
 }
